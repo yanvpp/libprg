d3cecac0c2b4d3807f4e81b912560c27ac2574c1
yanvpp <yanvitor.pp@gmail.com>
2025-09-25 11:17:37 -0300

    função adicionar na lista encadeada

--- libprg/src/libprg/lista_encadeada.c
+++ libprg/src/libprg/lista_encadeada.c
@@ -1,17 +1,25 @@
 //
 // Created by aluno on 25/09/2025.
 //
+#include <iso646.h>
+
 #include "libprg/libprg.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-no_t criar_no(int valor) {
-
+no_t *criar_lista_encadeada(int valor) {
     no_t *no = malloc(sizeof(no_t));
     no->valor = valor;
     no->proximo = NULL;
 
-    return *no;
+    return no;
 }
 
+no_t *adicionar_na_lista_encadeada(no_t *inicio, int valor) {
+    no_t *novo_no = criar_lista_encadeada(valor);
+    novo_no->proximo = inicio;
+    inicio = novo_no;
+
+    return inicio;
+}
