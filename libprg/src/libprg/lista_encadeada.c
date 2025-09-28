28c2c02c89f61ebf48f3209854e4fed4efd00a36
yanvpp <yanvitor.pp@gmail.com>
2025-09-28 11:53:50 -0300

    comentado

--- libprg/src/libprg/lista_encadeada.c
+++ libprg/src/libprg/lista_encadeada.c
@@ -1,25 +1,22 @@
+// #include <iso646.h>
 //
-// Created by aluno on 25/09/2025.
+// #include "libprg/libprg.h"
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <stdbool.h>
 //
-#include <iso646.h>
-
-#include "libprg/libprg.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
-no_t *criar_lista_encadeada(int valor) {
-    no_t *no = malloc(sizeof(no_t));
-    no->valor = valor;
-    no->proximo = NULL;
-
-    return no;
-}
-
-no_t *adicionar_na_lista_encadeada(no_t **inicio, int valor) {
-    no_t *novo_no = criar_lista_encadeada(valor);
-    novo_no->proximo = &inicio;
-    inicio = novo_no;
-
-    return inicio;
-}
+// no_t *criar_lista_encadeada(int valor) {
+//     no_t *no = malloc(sizeof(no_t));
+//     no->valor = valor;
+//     no->proximo = NULL;
+//
+//     return no;
+// }
+//
+// no_t *adicionar_na_lista_encadeada(no_t **inicio, int valor) {
+//     no_t *novo_no = criar_lista_encadeada(valor);
+//     novo_no->proximo = &inicio;
+//     inicio = novo_no;
+//
+//     return inicio;
+// }
