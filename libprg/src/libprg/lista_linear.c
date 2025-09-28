e00e76b1cc152c9cc0acf3fdf1ee21e322e8ea0f
yanvpp <yanvitor.pp@gmail.com>
2025-09-28 15:07:34 -0300

    att

--- libprg/src/libprg/lista_linear.c
+++ libprg/src/libprg/lista_linear.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-lista_linear_t *criar_lista(int capacidade, bool ordenada) {
-    lista_linear_t *ll = malloc(sizeof(lista_linear_t));
+lista_linear_t* criar_lista(int capacidade, bool ordenada)
+{
+    lista_linear_t* ll = malloc(sizeof(lista_linear_t));
 
     ll->elementos = malloc(capacidade * sizeof(int));
     ll->tamanho = 0;
@@ -13,79 +14,123 @@ lista_linear_t *criar_lista(int capacidade, bool ordenada) {
     return ll;
 }
 
-bool lista_cheia(lista_linear_t *ll) {
+bool lista_cheia(lista_linear_t* ll)
+{
     return ll->tamanho == ll->capacidade;
 }
 
-bool lista_vazia(lista_linear_t *ll) {
+bool lista_vazia(lista_linear_t* ll)
+{
     return ll->tamanho == 0;
 }
 
-void inserir_na_lista_nao_ordenada(lista_linear_t *ll, int valor) {
+void inserir_na_lista_nao_ordenada(lista_linear_t* ll, int valor)
+{
     ll->elementos[ll->tamanho] = valor;
     ll->tamanho++;
 }
 
-void inserir_na_lista_ordenada(lista_linear_t *ll, int valor) {
-    for (int i = ll->tamanho - 1; i >= 0; i--) {
-        if (ll->elementos[i] < valor) {
-            ll->elementos[i+1] = valor;
+void inserir_na_lista_ordenada(lista_linear_t* ll, int valor)
+{
+    for (int i = ll->tamanho - 1; i > 0; i--)
+    {
+        if (ll->elementos[i] < valor)
+        {
+            ll->elementos[i + 1] = valor;
             break;
         }
-        ll->elementos[i+1] = ll->elementos[i];
+        ll->elementos[i + 1] = ll->elementos[i];
     }
     ll->tamanho++;
 }
 
-void inserir_na_lista(lista_linear_t *ll, int valor) {
-    if (!lista_cheia(ll)) {
-        if (ll->ordenada) {
+void inserir_na_lista(lista_linear_t* ll, int valor)
+{
+    if (!lista_cheia(ll))
+    {
+        if (ll->ordenada)
+        {
             inserir_na_lista_ordenada(ll, valor);
-        } else {
+        }
+        else
+        {
             inserir_na_lista_nao_ordenada(ll, valor);
         }
     }
 }
 
-int busca_binaria(lista_linear_t *ll, int valor) {
+int busca_binaria(lista_linear_t* ll, int valor)
+{
     int index = ll->tamanho;
 
-    while (ll->elementos[index] != valor) {
-        if (valor > ll->elementos[index]) {
+    while (ll->elementos[index] != valor)
+    {
+        if (valor > ll->elementos[index])
+        {
             index = (ll->tamanho - index) % 2;
-        } else if (ll->elementos[index] < valor) {
+        }
+        else if (ll->elementos[index] < valor)
+        {
             index = ll->tamanho - (index % 2) + index;
         }
     }
     return index;
 }
 
-int busca_linear(lista_linear_t *ll, int valor) {
-    for (int index = 0; index < ll->tamanho; index++) {
-        if (ll->elementos[index] == valor) {
+int busca_linear(lista_linear_t* ll, int valor)
+{
+    for (int index = 0; index < ll->tamanho; index++)
+    {
+        if (ll->elementos[index] == valor)
+        {
             return index;
         }
-        return -1;
     }
+    return -1;
 }
 
-int buscar_na_lista(lista_linear_t *ll, int valor) {
-    if (!lista_vazia(ll)) {
-        busca_binaria(ll, valor);
+int buscar_na_lista(lista_linear_t* ll, int valor)
+{
+    if (!lista_vazia(ll))
+    {
+        if (ll->ordenada)
+        {
+            return busca_binaria(ll, valor);
+        }
+        return busca_linear(ll, valor);
     }
-        return 0;
+    return -1;
 }
 
-void remover_da_lista(lista_linear_t *ll, int valor) {
-    int index;
-
-    if (lista_vazia(ll)) {
-        index = buscar_na_lista(ll, valor);
-        ll->elementos[index] = ll->elementos[ll->tamanho];
+void remover_da_lista(lista_linear_t* ll, int valor)
+{
+    if (lista_vazia(ll))
+    {
+        return;
     }
+
+    int index = buscar_na_lista(ll, valor);
+
+    if (index == -1)
+    {
+        return;
+    }
+
+    if (ll->ordenada)
+    {
+        for (int i = index; i < ll->tamanho; i++)
+        {
+            ll->elementos[index] = ll->elementos[i + 1];
+        }
+    } else
+    {
+        ll->elementos[index] = ll->elementos[ll->tamanho - 1];
+    }
+    ll->tamanho--;
 }
 
-void destruir_lista(lista_linear_t *ll) {
+void destruir_lista(lista_linear_t* ll)
+{
     free(ll->elementos);
     free(ll);
 }
