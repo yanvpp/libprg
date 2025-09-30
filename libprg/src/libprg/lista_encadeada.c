0b1ae728798cf80955b860b234e0e41f59b25a38
yanvpp <yanvitor.pp@gmail.com>
2025-09-30 11:15:52 -0300

    att

--- libprg/src/libprg/lista_encadeada.c
+++ libprg/src/libprg/lista_encadeada.c
@@ -8,12 +8,20 @@
 typedef struct no {
     int valor;
     struct no *proximo;
-}no_t;
+} no_t;
 
 no_t *criar_lista_encadeada(int valor) {
     no_t *no = malloc(sizeof(no_t));
     no->valor = valor;
     no->proximo = NULL; // o último nó da lista sempre apontará para NULL
+
+    return no;
+}
+
+no_t *criar_lista_encadeada_circular(int valor) {
+    no_t *no = malloc(sizeof(no_t));
+    no->valor = valor;
+    no->proximo = no; // o último nó da lista sempre apontará para o primeiro elemento
 
     return no;
 }
@@ -24,11 +32,19 @@ void adicionar_na_lista_encadeada(no_t **inicio, int valor) {
     *inicio = novo_no; // e o início passa a apontar para o novo nó
 }
 
-no_t* buscar_na_lista_encadeada(no_t **inicio, int valor) {
+void adicionar_na_lista_encadeada_circular(no_t **inicio, int valor) {
+    no_t *novo_no = criar_lista_encadeada_circular(valor); // cria a lista encadeada circular (nó)
+    novo_no->proximo = *inicio; // o novo nó passa a apontar para o elemento que já existia
+    *inicio = novo_no; // e o início passa a apontar para o novo nó
+}
+
+no_t *buscar_na_lista_encadeada(no_t **inicio, int valor) {
     no_t *atual = *inicio;
 
-    while (atual) { // enquanto o valor do atual for diferente de nulo - mesmo que while(atual!=NULL)
-        if (atual->valor == valor) { // se o valor do atual for igual ao valor
+    while (atual) {
+        // enquanto o valor do atual for diferente de nulo - mesmo que while(atual!=NULL)
+        if (atual->valor == valor) {
+            // se o valor do atual for igual ao valor
             return atual; // retorna o endereço onde o valor está armazenado
         }
         atual = atual->proximo; // caso não entre no if, passa para o próximo elemento da lista
@@ -37,7 +53,21 @@ no_t* buscar_na_lista_encadeada(no_t **inicio, int valor) {
 }
 
 void remover_da_lista_encadeada(no_t **inicio, int valor) {
-    no_t *no_para_remover = buscar_na_lista_encadeada(*inicio, valor);
+    no_t *atual = *inicio;
+    no_t *anterior = NULL;
 
-
+    while (atual) { // enquanto tiver um atual
+        if (atual->valor == valor) {
+            // se o valor do atual for igual ao valor solicitado
+            if (anterior) { // se tiver anterior
+                anterior->proximo = atual->proximo; // o próximo do anterior será o próximo do atual
+            } else { // se não tiver anterior
+                *inicio = atual->proximo;
+            }
+            free(atual); // libera o local da memória do elemento removido ~~ remove o elemento
+            break; // se achou, quebra o while
+        }
+        anterior = atual;
+        atual = atual->proximo;
+    }
 }
