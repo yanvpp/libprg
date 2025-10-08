dfffc3ea858f094b141e666f4e44c312310d49d4
yanvpp <yanvitor.pp@gmail.com>
2025-10-08 19:54:58 -0300

    comentários

--- libprg/src/libprg/pilha.c
+++ libprg/src/libprg/pilha.c
@@ -1,88 +1,75 @@
 #include "libprg/libprg.h"
-#include <stdio.h>
 #include <stdlib.h>
 
-typedef struct pilha
+typedef struct pilha // definindo a struct pilha
 {
-    int *elementos;
-    int topo;
-    int capacidade;
+    int *elementos; // elementos da pilha
+    int topo; // topo da pilha (último elemento adicionado)
+    int capacidade; // quantidade máxima de elementos na pilha
 } pilha_t;
 
 pilha_t *criar_pilha(int capacidade) {
-    pilha_t *p = malloc(sizeof(pilha_t));
+    pilha_t *p = malloc(sizeof(pilha_t)); // aloca memória para a pilha ser criada
 
-    p->elementos = malloc(capacidade * sizeof(int));
-    p->topo = -1;
-    p->capacidade = capacidade;
+    p->elementos = malloc(capacidade * sizeof(int)); // aloca memória para os elementos
+    p->topo = -1; //o topo passa a ser -1 (pilha vazia, pois o primiero índice é zero)
+    p->capacidade = capacidade; // quantidade máxima de elementos na pilha
 
-    return p;
+    return p; // retorna a estrutura pilha criada
 }
 
 int empilhar_pilha(pilha_t *p, int valor)
 {
-    if (p->topo < p->capacidade - 1)
+    if (p->topo < p->capacidade - 1) // se a pilha ainda tiver espaço
     {
-        p->topo++;
-        p->elementos[p->topo] = valor;
-        return 1;
+        p->topo++; // o topo passa uma casa para frente
+        p->elementos[p->topo] = valor; // o novo elemento é adicionado no topo
+        return 1; // retorna que a operação foi bem sucedida
     }
-    else
-    {
-        return 0;
-    }
+        return 0; // retorna que a operação não foi concluída com sucesso
 }
 
 int desempilhar_pilha(pilha_t *p)
 {
-    if (p->topo >= 0)
+    if (p->topo >= 0) // se a pilha não estiver vazia
     {
-        int elemento = p->elementos[p->topo];
-        p->topo--;
-        return elemento;
+        int elemento = p->elementos[p->topo]; // armazena o elemento que está no topo em outra variável
+        p->topo--; // o topo retorna uma casa
+        return elemento; // retorna o valor que estava no topo
     }
-    else
-    {
-        return -1;
-    }
+        return -1; // retorna que a pilha está vazia
 }
 
 int topo_pilha(pilha_t *p)
 {
-    if (p->topo >= 0)
+    if (p->topo >= 0) // se a pilha não estiver vazia
     {
-        return p->elementos[p->topo];
+        return p->elementos[p->topo]; // retorna o elemento que está no topo da pilha
     }
-    else
-    {
-        return -1;
-    }
+        return -1; // retorna que a pilha está vazia
 }
 
 int* listar_pilha(pilha_t *p)
 {
-    int* cc = malloc(sizeof(int) * (p->topo + 1));
-    for (int i = 0; i <= p->topo; i++)
+    int* copia_do_elemento = malloc(sizeof(int) * (p->topo + 1)); // aloca memória para o array com a cópia dos elementos da pilha
+    for (int i = 0; i <= p->topo; i++) // enquanto i for menor que o topo
     {
-        cc[i] = p->elementos[i];
+        copia_do_elemento[i] = p->elementos[i]; // copia os elementos que estão na pilha para o novo array
     }
-    return cc;
+    return copia_do_elemento; // retorna o array com os mesmos elementos da pilha
 }
 
 int tamanho_pilha(pilha_t *p)
 {
-    if (p->topo >= 0)
+    if (p->topo >= 0) // se a pilha não estiver vazia
     {
-        return p->topo + 1;
+        return p->topo + 1; // retorna o tamanho da pilha
     }
-    else
-    {
-        return -1;
-    }
+        return -1; // se estiver vazia, retorna que está vazia
 }
 
 void destruir_pilha(pilha_t *p)
 {
-    free(p->elementos);
-    free(p);
+    free(p->elementos); // libera a memória alocada pelos elementos da pilha
+    free(p); // libera a memória alocada para a pilha
 }
